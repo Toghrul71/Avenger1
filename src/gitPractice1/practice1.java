4804fac4228ae89986ae50318c8bd5acb932e9fb
Toghrul <te7999@gmail.com>
2022-01-19 22:29:14 -0800

    new add and divide methods added

--- src/gitPractice1/practice1.java
+++ src/gitPractice1/practice1.java
@@ -4,4 +4,12 @@ public class practice1 {
     public static void main(String[] args) {
 
     }
+    public static int add(int num1, int num2){
+        int result=num1+num2;
+        return result;
+    }
+    public static int divide(int num1, int num2){
+        int result=num1/num2;
+        return result;
+    }
 }
